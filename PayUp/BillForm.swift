ff0d3d0a33714d3d4f90b5f47f1c145d7feafc95
iamhx <choohongxuan@gmail.com>
2017-04-20 01:02:13 +0800

    Update add button function

--- PayUp/BillForm.swift
+++ PayUp/BillForm.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-/*struct Person {
-	
-	let name : String
-	var amount : Double
-}*/
 
 struct Person {
 	
-	let name : String
-	var amount : Decimal
+	let name: String
+	var amount: Decimal
+}
+
+struct Person2 {
+	
+	let name: String
+	var item: [(itemName: String, price: Decimal)]
 }
 
 class cellPerson : UITableViewCell {
@@ -26,20 +27,24 @@ class cellPerson : UITableViewCell {
 	@IBOutlet weak var lblAmount: UILabel!
 }
 
-class BillForm: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+class BillForm: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
 
-	
 	@IBOutlet weak var lblTotal: UILabel!
 	@IBOutlet weak var billTableView: UITableView!
 
 	var structArray:[Person] = []
-	var totalBeforeTax:Decimal = 0.00
-	var controllerStyle : UIAlertControllerStyle?
+	
+	var pickerViewRow: Int = 0
+	let pickerPerson = UIPickerView()
+	var structArray2: [Person2] = []
+	var totalBeforeTax: Decimal = 0.00
+	var controllerStyle: UIAlertControllerStyle?
 	
 	@IBOutlet weak var switchSvcCharge: UISwitch!
 	@IBOutlet weak var switchGST: UISwitch!
 	
 	@IBAction func btnSplit(_ sender: UIButton) {
+		
 		
 		if (structArray.count <= 0 || billTableView.numberOfRows(inSection: 0) <= 0) {
 			
@@ -77,6 +82,14 @@ class BillForm: UIViewController, UITableViewDataSource, UITableViewDelegate, UI
 		}
 	}
 	
+	func textFieldDidBeginEditing(_ textField: UITextField) {
+		
+		if (textField.tag == 2)
+		{
+			textField.text = structArray2[pickerViewRow].name
+		}
+	}
+	
 	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 		
 		var maxLength:Int
@@ -96,27 +109,48 @@ class BillForm: UIViewController, UITableViewDataSource, UITableViewDelegate, UI
 		return newString.length <= maxLength
 	}
 	
-	@IBAction func addPerson(_ sender: UIBarButtonItem) {
+	func numberOfComponents(in pickerView: UIPickerView) -> Int {
+		return 1
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		return structArray2.count
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		return structArray2[row].name
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
 		
-		let inputPerson = UIAlertController(title: "Add Person", message: "Enter person's name and amount.", preferredStyle: .alert)
+		pickerViewRow = row
+		print(pickerViewRow)
+	}
+	
+	@IBAction func chooseOption(_ sender: UIBarButtonItem) {
 		
-		inputPerson.addTextField(configurationHandler: { (textField) -> Void in
+		//let inputPerson = UIAlertController(title: "Add Person", message: "Enter person's name and amount.", preferredStyle: .alert)
+		let chooseOption = UIAlertController(title: "Choose an option", message: "", preferredStyle: .alert)
+		let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+		
+		/*inputPerson.addTextField(configurationHandler: { (textField) -> Void in
 			textField.placeholder = "Name"
 			textField.textAlignment = .left
 			textField.keyboardType = .default
 			textField.delegate = self
 			textField.tag = 0
-		})
+		})*/
 		
-		inputPerson.addTextField(configurationHandler: { (textField) -> Void in
+		/*inputPerson.addTextField(configurationHandler: { (textField) -> Void in
 			textField.placeholder = "Amount"
 			textField.textAlignment = .left
 			textField.keyboardType = .decimalPad
 			textField.delegate = self
 			textField.tag = 1
-		})
+		})*/
 		
-		let actionAddPerson = UIAlertAction(title: "Add", style: .default, handler: { alert -> Void in
+		/*let actionAddPerson = UIAlertAction(title: "Add", style: .default, handler: { alert -> Void in
 			
 			let inputName = inputPerson.textFields![0] as UITextField
 			let inputAmt = inputPerson.textFields![1] as UITextField
@@ -145,15 +179,138 @@ class BillForm: UIViewController, UITableViewDataSource, UITableViewDelegate, UI
 					self.updateTotalLabel(label: self.lblTotal, amount: NSDecimalNumber(decimal: self.totalBeforeTax))
 				}
 			}
+		})*/
+		
+		let chooseAddPerson = UIAlertAction(title: "Add Person", style: .default, handler: { alert -> Void in
+			
+			let inputPerson = UIAlertController(title: "Add Person", message: "Enter the name of the person.", preferredStyle: .alert)
+			
+			inputPerson.addTextField(configurationHandler: { (textField) -> Void in
+				textField.placeholder = "Name"
+				textField.textAlignment = .left
+				textField.keyboardType = .default
+				textField.delegate = self
+				textField.tag = 0
+			})
+			
+			let actionAddPerson = UIAlertAction(title: "Add", style: .default, handler: { action in
+				
+				let inputName = inputPerson.textFields![0] as UITextField
+				
+				if (inputName.text!.isEmpty) {
+					
+					self.valError(promptInputAgain: inputPerson)
+				}
+				else {
+					
+					let newPerson = Person2(name: inputName.text!, item: [])
+					self.structArray2.append(newPerson)
+					
+					//self.billTableView.insertRows(at: [IndexPath(row: self.structArray2.count - 1, section: 0)], with: .automatic)
+					//self.billTableView.endUpdates()
+					print(self.structArray2[self.structArray2.count - 1])
+				}
+			})
+			
+			inputPerson.addAction(actionAddPerson)
+			inputPerson.addAction(actionCancel)
+			self.present(inputPerson, animated: true, completion: nil)
+		})
+
+		
+		let chooseAddItem = UIAlertAction(title: "Add Item", style: .default, handler: { action in
+
+			let pickPerson = UIAlertController(title: "Choose Person", message: "Choose the person to add the item to.", preferredStyle: .alert)
+			
+			pickPerson.addTextField(configurationHandler: { (textField) -> Void in
+				textField.placeholder = "Choose a Person"
+				textField.textAlignment = .left
+				textField.inputView = self.pickerPerson
+				textField.delegate = self
+				textField.tag = 2
+			})
+			
+			let choosePerson = UIAlertAction(title: "Choose", style: .default, handler: { action in
+			
+				let inputItem = UIAlertController(title: "Add Item", message: "Enter the item's name, price and choose the person to add to.", preferredStyle: .alert)
+				
+				inputItem.addTextField(configurationHandler: { (textField) -> Void in
+					textField.placeholder = "Item Name"
+					textField.textAlignment = .left
+					textField.keyboardType = .default
+					textField.delegate = self
+					textField.tag = 0
+				})
+				
+				inputItem.addTextField(configurationHandler: { (textField) -> Void in
+					textField.placeholder = "Price"
+					textField.textAlignment = .left
+					textField.keyboardType = .decimalPad
+					textField.delegate = self
+					textField.tag = 1
+				})
+				
+				let actionAddItem = UIAlertAction(title: "Add", style: .default, handler: { action in
+					
+					let inputItemName = inputItem.textFields![0] as UITextField
+					let inputPrice = inputItem.textFields![1] as UITextField
+					
+					if (inputItemName.text!.isEmpty || inputPrice.text!.isEmpty) {
+						
+						self.valError(promptInputAgain: inputItem)
+					}
+					else {
+						
+						let unwrappedInput = NSDecimalNumber.init(string: inputPrice.text!)
+						
+						if (unwrappedInput == NSDecimalNumber.notANumber) {
+							self.valError(promptInputAgain: inputItem)
+						}
+						else {
+							
+							self.structArray2[self.pickerViewRow].item.append((itemName: inputItemName.text!, price: unwrappedInput.decimalValue))
+							
+							//self.totalBeforeTax += self.structArray2[self.pickerViewRow!].item[self.pickerViewRow!].price!
+							
+							//self.billTableView.insertRows(at: [IndexPath(row: self.structArray.count - 1, section: 0)], with: .automatic)
+							//self.billTableView.endUpdates()
+							//self.updateTotalLabel(label: self.lblTotal, amount: NSDecimalNumber(decimal: self.totalBeforeTax))
+							print(self.structArray2)
+
+						}
+					}
+				})
+				
+				inputItem.addAction(actionAddItem)
+				inputItem.addAction(actionCancel)
+				self.present(inputItem, animated: true, completion: nil)
+			})
+			
+			pickPerson.addAction(choosePerson)
+			pickPerson.addAction(actionCancel)
+			self.present(pickPerson, animated: true, completion: nil)
 		})
 		
-		let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-		
-		inputPerson.addAction(actionAddPerson)
-		inputPerson.addAction(actionCancel)
-		inputPerson.preferredAction = actionAddPerson
+		//inputPerson.addAction(actionAddPerson)
+		//inputPerson.addAction(actionCancel)
+		//inputPerson.preferredAction = actionAddPerson
 	
-		self.present(inputPerson, animated: true, completion: nil)
+		//self.present(inputPerson, animated: true, completion: nil)
+		
+		
+		if (self.structArray2.count <= 0) {
+			
+			chooseOption.addAction(chooseAddPerson)
+		}
+		else {
+			
+			chooseOption.addAction(chooseAddPerson)
+			chooseOption.addAction(chooseAddItem)
+
+		}
+		
+		chooseOption.addAction(actionCancel)
+		self.present(chooseOption, animated: true, completion: nil)
 	}
 	
 	func valError(promptInputAgain: UIAlertController) {
@@ -186,6 +343,8 @@ class BillForm: UIViewController, UITableViewDataSource, UITableViewDelegate, UI
 		
 		billTableView.delegate = self
 		billTableView.dataSource = self
+		pickerPerson.delegate = self
+		pickerPerson.delegate = self
 		billTableView.separatorInset = .zero
 		billTableView.layoutMargins = .zero
 		
